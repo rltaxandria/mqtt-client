9fe1757e81ba6ae49c6a1ef31a1acb2cbd8882b8
Ruben Labruyere <ruben.labruyere@taxandria.com>
2026-05-21 10:34:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,26 +4,52 @@ const client = mqtt.connect("mqtt://192.168.0.99:1883");
 const state = {
   then: 0,
   now: 0,
-  dt: 0
+  dt: 0,
+  interval: null,
+
+  sent: 0,
+  received: 0
 }
 
 client.on("connect", () => {
-  getValue("power", "fuse1_enabled");
+  for (let i = 0; i < 8; i++) {
+    const index = i + 1;
+
+    getValue(`power`, `fuse${index}_enabled`);
+    getValue(`power`, `fuse${index}_output_status`);
+    getValue(`power`, `fuse${index}_tripped_status`);
+    getValue(`power`, `fuse${index}_trip_current`);
+    getValue(`power`, `fuse${index}_voltage`);
+    getValue(`power`, `fuse${index}_current`);
+
+    getValue(`power`, `pt100_temp${index}`);
+    getValue(`power`, `relay${index}_enabled`);
+    getValue(`power`, `relay${index}_status`);
+  }
 
   // Set state
   state.then = new Date().getTime();
   state.now = state.then;
+  state.interval = setInterval(() => {
+    if (state.sent === state.received) {
+      console.log(``);
+      console.log(`SENT: ${state.sent}`);
+      console.log(`RECEIVED: ${state.received}`);
+      console.log(`TIME: ${state.dt}`);
+
+      client.end();
+      clearInterval(state.interval);
+    }
+  }, 100);
 });
 
 client.on("message", (topic, message) => {
-  state.now = new Date().getTime();
-  state.dt = state.now - state.then;
-  state.then = state.now;
+  state.then = new Date().getTime();
+  state.dt = state.then - state.now;
+  state.received++;
 
-  console.log(state.dt);
-  console.log(`${topic}: ${message}`);
-
-  client.end();
+  // console.log(state.dt);
+  console.log(`${topic} => ${message}`);
 });
 
 function getValue(board, param) {
@@ -37,31 +63,28 @@ function setValue(board, param, value) {
 }
 
 function subscribe(board, param) {
-  const baseTopic = `board/${board}/parameter/${param}/res`;
-  const topic = `${baseTopic}/value`;
-
-  client.subscribe(topic);
+  client.subscribe(`board/${board}/parameter/${param}/res`);
 }
 
 function publishGet(board, param) {
-  const baseTopic = `board/${board}/parameter/${param}/req`;
-  const topic = `${baseTopic}/cmd`;
+  const topic = `board/${board}/parameter/${param}/req`;
 
   const cmd = {
-    cmd: "get",
+    cmd: "GET",
   };
 
   client.publish(topic, JSON.stringify(cmd));
+  state.sent++;
 }
 
 function publishSet(board, param, value) {
-  const baseTopic = `boards/${board}/parameter/${param}/req`;
-  const topic = `${baseTopic}/cmd`;
+  const topic = `boards/${board}/parameter/${param}/req`;
 
   const cmd = {
-    cmd: "set",
+    cmd: "SET",
     value: value
   };
 
   client.publish(topic, JSON.stringify(cmd));
+  state.sent++;
 }
